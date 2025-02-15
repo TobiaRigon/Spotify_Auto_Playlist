4e185f6e77d9b4027cc043873e28f5f2866a929a
S-SARAL <serkan.saral67@gmail.com>
2025-02-15 09:50:45 +0300

    AI playlist.py

--- python/playlist.py
+++ python/playlist.py
@@ -9,24 +9,8 @@ playlist_public = True
 
 # Lista di tracce da cercare e aggiungere alla playlist
 tracks_to_search = [
-    'Summertime Ella Fitzgerald & Louis Armstrong',
-    'Under the Boardwalk The Drifters',
-    'Cool Change Little River Band',
-    'Here Comes the Sun The Beatles',
-    'Island in the Sun Weezer',
-    'Summer Breeze Seals & Crofts',
-    'Walking on Sunshine Katrina and the Waves',
-    'Heat Wave Martha Reeves & The Vandellas',
-    'Summer in the City The Lovin\' Spoonful',
-    'Sunshine Reggae Laid Back',
-    'Kokomo The Beach Boys',
-    'Sunny Boney M.',
-    'Groovin\' The Young Rascals',
-    'In the Summertime Mungo Jerry',
-    'Hot Fun in the Summertime Sly & The Family Stone',
-    'Tequila Sunrise Eagles',
-    'California Dreamin\' The Mamas & The Papas',
-    'Good Vibrations The Beach Boys',
-    'Lovely Day Bill Withers',
-    'Cruel Summer Bananarama'
+    'Blinding Lights - The Weeknd',
+    'Shape of You - Ed Sheeran',
+    'Levitating - Dua Lipa',
+    'Stay - The Kid LAROI, Justin Bieber'
 ]
